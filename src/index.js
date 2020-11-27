8fd29d74528ad22f2ff9ffdd04e6a7d194c805f9
mina <surasan@hotmail.fr>
2020-11-27 17:57:26 +0100

    footerHeaderCommit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log(" nodemon ça marche tt seul Seul ")
+console.log("js fonctionne");
+
+$(document).ready(function(){   
+     
+    //Dès qu'on clique sur #b1, on applique hide() au titre
+    $("#b2").click(function(){
+        $("ul.list").hide();
+    });
+    
+    //Dès qu'on clique sur #b1, on applique show() au titre
+    $("#b1").click(function(){
+        $("ul.list").show();
+    });
+});
+
+
+
